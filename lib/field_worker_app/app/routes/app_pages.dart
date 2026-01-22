b2823d1634991e7e329b3eef5acd7b5eb5e5a743
Roo <roo@antigraviti.com>
2026-01-23 00:46:44 +0700

    fix: resolve GetX type mismatch by using package:get in AppPages

--- lib/field_worker_app/app/routes/app_pages.dart
+++ lib/field_worker_app/app/routes/app_pages.dart
@@ -1,7 +1,7 @@
 import 'package:field_technician_app/field_worker_app/app/modules/dashboard/dashboard_page.dart';
 import 'package:field_technician_app/field_worker_app/app/modules/login/login_page.dart';
 import 'package:field_technician_app/field_worker_app/app/modules/task/task_page.dart';
-import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 part 'app_routes.dart';
 
@@ -22,19 +22,4 @@ class AppPages {
       page: () => const TaskPage(),
     ),
   ];
-}
-
-class GetPage extends Page {
-  final String name;
-  final Widget Function() page;
-
-  GetPage({required this.name, required this.page});
-
-  @override
-  Route createRoute(BuildContext context) {
-    return MaterialPageRoute(
-      settings: this,
-      builder: (BuildContext context) => page(),
-    );
-  }
 }
